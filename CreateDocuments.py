4e8fccc5043eb20682d65c9d53ff1b182bb5496e
Ryan Kish <43594648+ryandouglaskish@users.noreply.github.com>
2024-04-08 13:09:36 -0500

    create documents cleaned

--- CreateDocuments.py
+++ CreateDocuments.py
@@ -9,16 +9,11 @@ RAW_TEST_DATA_PATH = 'rag-dataset-12000/data/test-00000-of-00001-af2a9f454ad1b8a
 
 def join_original_contexts(dataset_path):
     df = pd.read_parquet(dataset_path).head(2) # For testing purposes
-    text = df['context'].array
-    for i in range(len(text)):
-        assert '<sep>' not in text[i]
-    text = '<sep>'.join(text)
-    return text
+    return df['context'].array
 
 def split_into_documents(dataset_path):
     
     original_contexts = join_original_contexts(dataset_path)
-    original_contexts = original_contexts.split('<sep>')
 
     documents = []
     for original_context in original_contexts:
